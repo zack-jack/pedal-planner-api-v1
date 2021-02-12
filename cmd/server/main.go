5a2cf4c049a30bb2a9556c6aaa0053c32ef852ce
zack-jack <zjackson@redventures.com>
2021-02-11 20:59:51 -0500

    hello world

---
+++ cmd/server/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(w, "Hello %s", d)
+	})
+
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zack-jack/pedal-tetris-api-v1
+
+go 1.15
